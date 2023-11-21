c2fdfd01b1c72a1f855159c8c43a163026917367
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2023-11-22 01:10:01 +0530

    Update leech_utils.py

--- bot/helper/ext_utils/leech_utils.py
+++ bot/helper/ext_utils/leech_utils.py
@@ -1,8 +1,3 @@
-import aiohttp
-import asyncio
-import aiofiles
-import re
-import requests
 from hashlib import md5
 from time import strftime, gmtime, time
 from re import sub as re_sub, search as re_search
@@ -117,30 +112,10 @@ async def get_audio_thumb(audio_file):
         return None
     return des_dir
 
-async def download_file(url, dest_path):
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url) as response:
-            async with aiofiles.open(dest_path, 'wb') as f:
-                while True:
-                    chunk = await response.content.read(1024)
-                    if not chunk:
-                        break
-                    await f.write(chunk)
-
 
 async def take_ss(video_file, duration=None, total=1, gen_ss=False):
     des_dir = ospath.join('Thumbnails', f"{time()}")
     await makedirs(des_dir, exist_ok=True)
-
-    movie_name, release_year = await extract_movie_info(ospath.basename(video_file))
-    if movie_name and release_year:
-        poster_url = await get_movie_poster(movie_name, release_year)
-        if poster_url:
-            # Download the movie poster
-            poster_file = ospath.join(des_dir, f"{time()}_poster.jpg")
-            await download_file(poster_url, poster_file)
-            return poster_file, None  # Return the poster file path and None for tstamps
-
     if duration is None:
         duration = (await get_media_info(video_file))[0]
     if duration == 0:
@@ -342,35 +317,3 @@ def get_md5_hash(up_path):
         for byte_block in iter(lambda: f.read(4096), b""):
             md5_hash.update(byte_block)
         return md5_hash.hexdigest()
-
-async def get_movie_poster(movie_name, release_year):
-    tmdb_api_key = '0dfbeb8ce49d198fb0bf99e08b6a8557'
-    tmdb_api_url = f'https://api.themoviedb.org/3/search/multi?api_key={tmdb_api_key}&query={movie_name}&year={release_year}'
-
-    try:
-        response = requests.get(tmdb_api_url)
-        data = response.json()
-
-        if data['results']:
-            poster_path = data['results'][0]['poster_path']
-            return f"https://image.tmdb.org/t/p/original{poster_path}"
-        else:
-            print(f"No results found for movie: {movie_name} ({release_year})")
-    except Exception as e:
-        print(f"Error fetching TMDB data: {e}")
-
-    return None
-
-
-async def extract_movie_info(caption):
-    try:
-        regex = re.compile(r'(.+?)(\d{4})')
-        match = regex.search(caption)
-
-        if match:
-             movie_name = match.group(1).replace('.', ' ').strip()
-             release_year = match.group(2)
-             return movie_name, release_year
-    except Exception as e:
-        print(e)
-    return None, None
